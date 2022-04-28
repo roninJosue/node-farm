b639c81351acc3859715bade452def1e2678f2fa
Reynaldo Josué Cano Bárcenas <roninjosue88@gmail.com>
2022-04-27 22:27:38 -0600

    Implementing slugify for construct better url

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 const fs = require('fs');
 const http = require('http');
 const url = require('url');
+const slugify = require('slugify');
 const replaceTemplate = require('./modules/replaceTemplate');
-
-///////////////////////////////
-/// SERVER
-
-
 
 const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8')
 const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8')
 const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8')
+
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8')
 const dataObj = JSON.parse(data)
+
+const slugs = dataObj.map(el => slugify(el.productName, { lower: true }))
 
 const server = http.createServer((req, res) => {
   const {pathname, searchParams} = new URL(req.url, `http://${req.headers.host}/`)
@@ -28,7 +27,7 @@ const server = http.createServer((req, res) => {
   } else if (pathname === '/product') {
     res.writeHead(200, {'Content-type': 'text/html'})
 
-    const product = dataObj[searchParams.get('id')]
+    const product = dataObj[slugs.indexOf(searchParams.get('id'))]
     const output = replaceTemplate(tempProduct, product)
 
     res.end(output)
--- modules/replaceTemplate.js
+++ modules/replaceTemplate.js
@@ -1,3 +1,5 @@
+const slugify = require('slugify');
+
 module.exports = (temp, product) => {
   let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
   output = output.replace(/{%IMAGE%}/g, product.image);
@@ -6,7 +8,7 @@ module.exports = (temp, product) => {
   output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
   output = output.replace(/{%QUANTITY%}/g, product.quantity);
   output = output.replace(/{%DESCRIPTION%}/g, product.description);
-  output = output.replace(/{%ID%}/g, product.id);
+  output = output.replace(/{%SLUG%}/g, slugify(product.productName, { lower: true }));
 
   if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
 
