9b2ebb4817f62a73aa36df601a407ef5a0bd2030
Reynaldo Josué Cano Bárcenas <roninjosue88@gmail.com>
2022-04-27 19:07:09 -0600

    Passing variable through url query

--- index.js
+++ index.js
@@ -2,29 +2,6 @@ const fs = require('fs');
 const http = require('http');
 const url = require('url');
 
-// Blocking, synchronous way
-//const textIn = fs.readFileSync("./txt/input.txt", "utf-8")
-//console.log(textIn)
-
-//const textOut = `This is what we know about the avocado:\n${textIn}.\nCreated on ${Date.now()}`
-//fs.writeFileSync("./txt/output.txt", textOut)
-
-// Non-blocking, asynchronous way
-/*console.log("Going to read a file")
-fs.readFile("./txt/start.txt", 'utf-8',(err, data1) => {
-    fs.readFile(`./txt/${data1}.txt`, 'utf-8',(err, data2) => {
-        console.log(data2)
-        fs.readFile("./txt/append.txt", 'utf-8',(err, data3) => {
-            console.log(data3)
-            fs.writeFile('./txt/final.txt', `${data2}\n${data3}`,'utf-8', err =>{
-                console.log("Your file has been written")
-            })
-        })
-    })
-})
-console.log("file read")*/
-
-
 ///////////////////////////////
 /// SERVER
 
@@ -50,20 +27,27 @@ const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8')
 const dataObj = JSON.parse(data)
 
 const server = http.createServer((req, res) => {
-  const pathName = req.url
-  if (pathName === '/overview' || pathName === '/') {
+  const {pathname, searchParams} = new URL(req.url, `http://${req.headers.host}/`)
+  console.log(pathname, searchParams)
+  console.log(`id: ${searchParams.get('id')}`)
+  if (pathname === '/overview' || pathname === '/') {
     res.writeHead(200, {
       'Content-type': 'text/html'
     })
 
     const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('')
     const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml)
-    console.log(cardsHtml)
+    //console.log(cardsHtml)
 
     res.end(output)
-  } else if (pathName === '/product') {
-    res.end('Product page')
-  } else if (pathName === '/api') {
+  } else if (pathname === '/product') {
+    res.writeHead(200, {'Content-type': 'text/html'})
+
+    const product = dataObj[searchParams.get('id')]
+    const output = replaceTemplate(tempProduct, product)
+
+    res.end(output)
+  } else if (pathname === '/api') {
     res.writeHead(200, {
       'Content-type': 'application/json'
     })
