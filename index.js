c162b1e045db10e10c3e1c51a514196d449afedb
Reynaldo Josué Cano Bárcenas <roninjosue88@gmail.com>
2022-04-27 21:34:53 -0600

    Creating a module

--- index.js
+++ index.js
@@ -1,24 +1,12 @@
 const fs = require('fs');
 const http = require('http');
 const url = require('url');
+const replaceTemplate = require('./modules/replaceTemplate');
 
 ///////////////////////////////
 /// SERVER
 
-const replaceTemplate = (temp, product) => {
-  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-  output = output.replace(/{%IMAGE%}/g, product.image);
-  output = output.replace(/{%PRICE%}/g, product.price);
-  output = output.replace(/{%FROM%}/g, product.from);
-  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-  output = output.replace(/{%QUANTITY%}/g, product.quantity);
-  output = output.replace(/{%DESCRIPTION%}/g, product.description);
-  output = output.replace(/{%ID%}/g, product.id);
 
-  if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
-
-  return output;
-}
 
 const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8')
 const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8')
@@ -28,8 +16,6 @@ const dataObj = JSON.parse(data)
 
 const server = http.createServer((req, res) => {
   const {pathname, searchParams} = new URL(req.url, `http://${req.headers.host}/`)
-  console.log(pathname, searchParams)
-  console.log(`id: ${searchParams.get('id')}`)
   if (pathname === '/overview' || pathname === '/') {
     res.writeHead(200, {
       'Content-type': 'text/html'
@@ -37,7 +23,6 @@ const server = http.createServer((req, res) => {
 
     const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('')
     const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml)
-    //console.log(cardsHtml)
 
     res.end(output)
   } else if (pathname === '/product') {
@@ -52,7 +37,6 @@ const server = http.createServer((req, res) => {
       'Content-type': 'application/json'
     })
     res.end(data)
-    //res.end('API')
   } else {
     res.writeHead(404, {
       'Content-Type': 'text/html'
