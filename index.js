7daad5d702b499c7ebea005fe747a2ee56740334
Reynaldo Josué Cano Bárcenas <roninjosue88@gmail.com>
2022-04-30 22:08:12 -0600

    Configurating prettier

--- index.js
+++ index.js
@@ -4,49 +4,60 @@ const slugify = require('slugify');
 
 const replaceTemplate = require('./modules/replaceTemplate');
 
-const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8')
-const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8')
-const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8')
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  'utf-8'
+);
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  'utf-8'
+);
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  'utf-8'
+);
 
-const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8')
-const dataObj = JSON.parse(data)
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const dataObj = JSON.parse(data);
 
-const slugs = dataObj.map(el => slugify(el.productName, { lower: true }))
+const slugs = dataObj.map((el) => slugify(el.productName, { lower: true }));
 
 const server = http.createServer((req, res) => {
-  const {pathname, searchParams} = new URL(req.url, `http://${req.headers.host}/`)
+  const { pathname, searchParams } = new URL(
+    req.url,
+    `http://${req.headers.host}/`
+  );
   if (pathname === '/overview' || pathname === '/') {
     res.writeHead(200, {
-      'Content-type': 'text/html'
-    })
+      'Content-type': 'text/html',
+    });
 
-    const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('')
-    const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml)
+    const cardsHtml = dataObj
+      .map((el) => replaceTemplate(tempCard, el))
+      .join('');
+    const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
 
-    res.end(output)
-
+    res.end(output);
   } else if (pathname === '/product') {
-    res.writeHead(200, {'Content-type': 'text/html'})
+    res.writeHead(200, { 'Content-type': 'text/html' });
 
-    const product = dataObj[slugs.indexOf(searchParams.get('id'))]
-    const output = replaceTemplate(tempProduct, product)
+    const product = dataObj[slugs.indexOf(searchParams.get('id'))];
+    const output = replaceTemplate(tempProduct, product);
 
-    res.end(output)
-
+    res.end(output);
   } else if (pathname === '/api') {
     res.writeHead(200, {
-      'Content-type': 'application/json'
-    })
-    res.end(data)
-
+      'Content-type': 'application/json',
+    });
+    res.end(data);
   } else {
     res.writeHead(404, {
-      'Content-Type': 'text/html'
-    })
-    res.end('<h1>Page not found!!</h1>')
+      'Content-Type': 'text/html',
+    });
+    res.end('<h1>Page not found!!</h1>');
   }
 });
 
 server.listen(3200, '127.0.0.1', () => {
-  console.log("Listening on port 3200")
-})
+  console.log('Listening on port 3200');
+});
